edfb52721325cacd291103f7b1aa1764d87b8470
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-04-18 01:17:37 +0500

    login functionality complete

--- LoginSignup/app/src/main/java/com/example/loginsignup/MainActivity.kt
+++ LoginSignup/app/src/main/java/com/example/loginsignup/MainActivity.kt
@@ -4,19 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
-    fun changeScreen(view : View){
-        val intent : Intent = Intent(this, Signup::class.java)
-        startActivity(intent)
-    }
-    fun changeScreen4(view: View){
-        val intent2 : Intent = Intent(this, WelcomScreen::class.java)
+    fun signup(view : View){
+        val intent2 : Intent = Intent(this, Signup::class.java)
         startActivity(intent2)
+
+    }
+    fun login(view: View){
+        val txtUsername = findViewById<EditText>(R.id.editTextUsername)
+        val txtPassword = findViewById<EditText>(R.id.editTextPassword)
+
+        val username = txtUsername.text.toString()
+        val password = txtPassword.text.toString()
+
+        if(username == "waheed" && password == "abc123"){
+            val intent : Intent = Intent(this, WelcomScreen::class.java)
+            intent.putExtra("name",username)
+            startActivity(intent)
+        }
+        else{
+            Toast.makeText(this,"Incorrect Username or Password",Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- LoginSignup/app/src/main/java/com/example/loginsignup/WelcomScreen.kt
+++ LoginSignup/app/src/main/java/com/example/loginsignup/WelcomScreen.kt
@@ -4,14 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 
 class WelcomScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcom_screen)
+
+        val lblUsername = findViewById<TextView>(R.id.lblUsername)
+        val intent : Intent = getIntent()
+        val name = intent.getStringExtra("name")
+        lblUsername.text = "Mr. $name"
     }
 
-    fun changeScreen3(view: View){
+    fun logout(view: View){
         val intent : Intent = Intent(this,MainActivity::class.java)
         startActivity(intent)
     }
