5550fa337a88c30b2fd58b1723d8c0697364ea56
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-05-13 17:10:32 +0500

    madLibs - design

--- MadLibsStoryGame/app/src/main/java/com/example/madlibs_storygame/MainActivity.kt
+++ MadLibsStoryGame/app/src/main/java/com/example/madlibs_storygame/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.madlibs_storygame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun getStarted(view: View){
+        val intent : Intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
     }
 }
