bfc0bf9cf6170e04f5af88328c70ab1961ae32f2
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-05-16 16:24:31 +0500

    lab4Task1-complete

---
+++ lab4Task1/app/src/main/java/com/example/lab4task1/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.lab4task1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+}
